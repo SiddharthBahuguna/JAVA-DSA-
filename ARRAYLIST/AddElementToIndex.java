3bdd16a420b4c96e0e56c606e856bcf80fe147dd
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-05-15 00:03:51 +0530

    Add files via upload

---
+++ ARRAYLIST/AddElementToIndex.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class AddElementToIndex {
+    public static void main(String args[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        list.add(1, 9);
+        System.out.println(list);
+    }
+
+}
