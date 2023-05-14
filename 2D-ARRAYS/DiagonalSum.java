26490ff9a8351cac48c37e0252dea5443c4dc5fa
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-05-14 23:41:09 +0530

    Add files via upload

---
+++ 2D-ARRAYS/DiagonalSum.java
@@ -0,0 +1,35 @@
+public class DiagonalSum {
+    public static int diagonalSum(int matrix[][]) {
+        int sum = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            sum += matrix[i][i]; // primary diagonal
+
+            if (i != matrix.length - 1 - i)
+                sum += matrix[i][matrix.length - i - 1];
+        }
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        int matrix[][] = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
+        System.out.println(diagonalSum(matrix));
+    }
+
+}
+
+/* 
+for (int i = 0; i < matrix.length; i++) {
+    for (int j = 0; j < matrix[0].length; j++) {
+        if (i == j) {
+            sum += matrix[i][j];
+        } else if (i + j == matrix.length - 1)
+
+        {
+            sum += matrix[i][j];
+        }
+    }
+}
