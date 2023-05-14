52d91c8c380777a3c95787ebefae6453eebae6b4
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-05-15 00:06:52 +0530

    Add files via upload

---
+++ ARRAYLIST/ContainerWithMostWater.java
@@ -0,0 +1,34 @@
+import java.util.*;  
+
+public class ContainerWithMostWater {
+    public static int storeWater(ArrayList<Integer> height) {
+        int maxWater = 0;
+        // brute force
+        for (int i = 0; i < height.size(); i++) {
+            for (int j = i + 1; j < height.size(); j++) {
+
+                int ht = Math.min(height.get(i), height.get(j));
+                int width = j - i;
+                int currWater = ht * width;
+                maxWater = Math.max(maxWater, currWater);
+            }
+        }
+        return maxWater;
+    }
+
+    public static void main(String args[]) {
+        ArrayList<Integer> height = new ArrayList<>();
+
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+
+        System.out.println(storeWater(height));
+    }
+}
