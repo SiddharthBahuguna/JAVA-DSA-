212688d92725f83fa15266cd9738b4db3cb8324b
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-05-17 00:29:50 +0530

    Add files via upload

---
+++ ARRAYLIST/PairSum1UsingTwoPointers.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class PairSum1UsingTwoPointers {
+    public static boolean pairSum1(ArrayList<Integer> list, int target) {
+        int lp = 0;
+        int rp = list.size() - 1;
+
+        while (lp != rp) {
+            // case 1
+            if (list.get(lp) + list.get(rp) == target) {
+                return true;
+            }
+
+            // case 2
+            if (list.get(lp) + list.get(rp) < target) {
+                lp++;
+            } else {
+                // case 3
+                rp--;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target = 5;
+        System.out.println(pairSum1(list, target));
+    }
+}
