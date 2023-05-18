bd81907df8f36bdd2420ff386d8b54d64c1b9930
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-05-18 22:03:32 +0530

    Add files via upload

---
+++ ARRAYLIST/SizeofArrayList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class SizeofArrayList {
+    public static void main(String args[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " "); // print array list
+        }
+    }
+
+}
