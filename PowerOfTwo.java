d81c0d63a1a91cc2df79875609ef83227862c6b0
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-05-22 11:45:48 +0530

    Delete PowerOfTwo.java

--- PowerOfTwo.java
+++
@@ -1,9 +0,0 @@
-public class PowerOfTwo {
-    public static boolean isPowerofTwo(int n) {
-        return (n & (n - 1)) == 0;
-    }
-
-    public static void main(String args[]);
-    System.out.println(isPowerofTwo(15));
-    
-}
