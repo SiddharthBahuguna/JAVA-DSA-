9f5f446fc16e3778ad3512bf9c11439d71499915
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-05-23 19:54:45 +0530

    Add files via upload

---
+++ RECURSION/Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * fact(n - 1);
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        System.out.println(fact(n));
+    }
+
+}
