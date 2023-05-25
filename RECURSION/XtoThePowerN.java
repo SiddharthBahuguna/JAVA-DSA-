ef181e398c15a0cda76ea4d04165386dcf6c56d5
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-05-25 20:12:46 +0530

    Add files via upload

---
+++ RECURSION/XtoThePowerN.java
@@ -0,0 +1,12 @@
+public class XtoThePowerN {
+    public static int power(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return x * power(x, n - 1);
+    }
+
+    public static void main(String args[]) {
+        System.out.println(power(2, 10));
+    }
+}
