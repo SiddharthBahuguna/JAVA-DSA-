e4a96fc1936cf86b6e94bd980909c033469e036f
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-05-29 22:07:51 +0530

    Add files via upload

---
+++ LINKED LIST/AddInLast.java
@@ -0,0 +1,49 @@
+public class AddInLast {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("Linked List is empty");
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+
+    public static void main(String args[]) {
+        AddInLast ll = new AddInLast();
+        ll.addLast(4);
+        ll.addLast(3);
+        ll.addLast(2);
+        ll.addLast(1);
+
+        ll.print();
+    }
+
+}
