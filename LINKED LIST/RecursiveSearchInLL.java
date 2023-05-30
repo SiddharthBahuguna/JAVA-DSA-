157df47861ff36803b5f0caa14c4bfee4a9e93ac
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-05-31 02:01:18 +0530

    Add files via upload

---
+++ LINKED LIST/RecursiveSearchInLL.java
@@ -0,0 +1,73 @@
+public class RecursiveSearchInLL {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+
+        head = newNode;
+    }
+
+    public int helper(Node head, int key) {
+        if (head == null) {
+            return -1;
+        }
+        if (head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if (idx == -1) {
+            return -1;
+        }
+        return idx + 1;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("Linked List is empty");
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+
+    public int recSearch(int key) {
+        return helper(head, key);
+    }
+
+    public static void main(String args[]) {
+        RecursiveSearchInLL ll = new RecursiveSearchInLL();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+
+        ll.print();
+
+        System.out.println(ll.recSearch(3));
+        System.out.println(ll.recSearch(10));
+
+    }
+}
