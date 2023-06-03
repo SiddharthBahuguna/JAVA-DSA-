d7044e3811df9c032abbb0e3fb36afcba51ad860
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-06-04 00:19:09 +0530

    Add files via upload

---
+++ GREEDY ALGORITHM/IndianCoins.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class IndianCoins {
+    public static void main(String args[]) {
+        Integer coins[] = { 1, 2, 5, 10, 20, 50, 100, 500, 2000 };
+
+        Arrays.sort(coins, Comparator.reverseOrder());
+
+        int countofCoins = 0;
+        int amount = 590;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= amount) {
+                while (coins[i] <= amount) {
+                    countofCoins++;
+                    ans.add(coins[i]);
+                    amount -= coins[i];
+                }
+            }
+        }
+        System.out.println("Total (min)coins used=" + countofCoins);
+
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+}
+/*
+ * Total (min)coins used=4
+ * 500 50 20 20
+ */
