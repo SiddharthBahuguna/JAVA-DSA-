6fac19c89aa1f0e4450d7a5b55281c5d7fb4c02e
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-06-04 00:21:39 +0530

    Add files via upload

---
+++ GREEDY ALGORITHM/MinSumAbsolute.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class MinSumAbsolute {
+    public static void main(String args[]) {
+        int A[] = { 1, 2, 3 };
+        int B[] = { 2, 1, 3 };
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int minDiff = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            minDiff += Math.abs(A[i] - B[i]);
+        }
+        System.out.println("Min absolute diff of pairs=" + minDiff);// Min absolute diff of pairs=0
+    }
+
+}
