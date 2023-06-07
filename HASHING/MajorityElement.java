6faf5518258d5375ff8043b7a0d88c095ef2319f
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-06-07 22:40:52 +0530

    Add files via upload

---
+++ HASHING/MajorityElement.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class MajorityElement {
+    public static void main(String args[]) {
+        int arr[] = { 1, 3, 2, 5, 1, 3, 1, 5, 1 };
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        Set<Integer> keySet = map.keySet();
+        for (Integer key : map.keySet()) {
+            if (map.get(key) > arr.length / 3) {
+                System.out.println(key);
+            }
+        }
+    }
+
+}
