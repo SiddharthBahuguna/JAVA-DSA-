9ad40fae9e21a092424efcefc1d56812d116adae
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-10-06 21:54:19 +0530

    Add files via upload

---
+++ DP/ZeroOneKnapsackTabulation.java
@@ -0,0 +1,43 @@
+public class ZeroOneKnapsackTabulation {
+    public static int knapsackTab(int val[], int wt[], int W) {
+        int n = val.length;
+        int dp[][] = new int[n + 1][W + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < dp[0].length; j++) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                int v = val[i - 1];
+                int w = wt[i - 1];
+
+                if (w <= j) {
+                    int incProfit = v + dp[i - 1][j - w];
+                    int excProfit = dp[i - 1][j];
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                } else {
+                    int excProfit = dp[i - 1][j];
+                    dp[i][j] = excProfit;
+                }
+            }
+        }
+        return dp[n][W];
+    }
+
+    public static void main(String args[]) {
+        int val[] = { 15, 14, 10, 45, 30 };
+        int wt[] = { 2, 5, 1, 3, 4 };
+        int W = 7;
+
+        int dp[][] = new int[val.length + 1][W + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(knapsackTab(val, wt, W));// 75
+    }
+
+}
