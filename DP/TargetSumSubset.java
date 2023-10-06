f428f516e5477f0e128cdfb0be4a9db0c53fc5ff
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-10-06 21:53:06 +0530

    Add files via upload

---
+++ DP/TargetSumSubset.java
@@ -0,0 +1,37 @@
+public class TargetSumSubset {
+    public static boolean targetSumSubset(int arr[], int sum) {
+        int n = arr.length;
+        boolean dp[] = new boolean[n + 1][sum + 1];
+        // i=items & j=target sum
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                int v = arr[i - 1];
+                // include
+                if (v <= j && dp[i - 1][j - v] == true) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        print(dp);
+        return dp[n][sum];
+    }
+
+    public static void print(boolean dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.println(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 4, 2, 7, 1, 3 };
+        int sum = 10;
+        System.out.println(targetSumSubset(arr, sum));
+    }
+}
