1b8d3b4f2196f0b7e293903f652b6bb61cc55229
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2023-10-24 21:53:29 +0530

    Add files via upload

---
+++ TRIES/LongestWordWIthAllPrefix.java
@@ -0,0 +1,80 @@
+public class LongestWordWIthAllPrefix {
+    static class Node {
+        Node[] children = new Node[26];
+        boolean eow = false;
+        int freq;
+
+        public Node() {
+            for (int i = 0; i < children.length; i++) {
+                children[i] = null;
+            }
+            freq = 1;
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+
+            else {
+                curr.children[idx].freq++;
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static boolean search(String key) {
+        int level = 0;
+        int len = key.length();
+        int idx = 0;
+
+        Node curr = root;
+        for (; level < len; level++) {
+            idx = key.charAt(level) - 'a';
+
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+    }
+
+    public static String ans = " ";
+
+    public static void longestWord(Node root, StringBuilder temp) {
+        if (root == null) {
+            return;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (root.children[i] != null && root.children[i].eow == true) {
+                char ch = (char) (i + 'a');
+                temp.append(ch);
+                if (temp.length() > ans.length()) {
+                    ans = temp.toString();
+                }
+                longestWord(root.children[i], temp);
+                temp.deleteCharAt(temp.length() - 1);// backtrack
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        String words[] = { "a", "banana", "app", "appl", "ap", "apply", "apple" };
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+        longestWord(root, new StringBuilder(" "));
+        System.out.println(ans);// apple
+    }
+
+}
