4bfcb7a37873c3a5b197074fe9ecc1d89385510d
SiddharthBahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2024-01-16 22:52:53 +0530

    Create Introduction To Linked List.java

---
+++ LINKED LIST(Complete)/Introduction To Linked List.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static Node constructLL(int []arr) {
+        // Write your code here
+        Node head=new Node(arr[0]);
+        Node mover=head;
+        for(int i=1;i<arr.length;i++){
+            Node temp=new Node(arr[i]);
+            mover.next=temp;
+            mover=temp;
+        }
+        return head;
+    }
+}
